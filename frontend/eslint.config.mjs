b3526659a3db6d59d5806698cf7e92dfc12f8b5f
ST10048211 <st10048211@vcconnect.edu.za>
2024-10-08 06:59:01 +0200

    Added Tests for Register

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -5,7 +5,7 @@ import pluginReact from "eslint-plugin-react";
 
 export default [
   {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
+  {files: ["**/*.js"], languageOptions: {sourceType: "script"}},
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
